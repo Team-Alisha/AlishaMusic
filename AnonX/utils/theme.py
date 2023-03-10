a414f943d68ffc27894f35dd507660b500a168a2
Team Alexa <110902508+TheTeamAlexa@users.noreply.github.com>
2023-03-10 15:06:00 +0500

    Update Thumbnail

---
+++ AnonX/utils/theme.py
@@ -0,0 +1,31 @@
+## this code is added by the (C) TheTeamAlexa on 7th March
+# Copyright (C) 2021-2022 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >.
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# All rights reserved. © Alisha © Alexa © Yukki
+
+
+import random
+from from AnonX.utils.database impor import get_theme
+
+themes = [
+    "Alisha",
+    "Alisha1",
+    "Alisha11",
+    "Alisha13",
+    "Alisha12",
+    "Alisha14",
+    "Alisha15",
+    "Alisha3",
+    "Alisha2",
+]
+
+
+async def check_theme(chat_id: int):
+    _theme = await get_theme(chat_id, "theme")
+    if not _theme:
+        theme = random.choice(themes)
+    else:
+        theme = _theme["theme"]
+        if theme == "Random":
+            theme = random.choice(themes)
+    return theme
