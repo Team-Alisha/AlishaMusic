a6015f883a3c837e77bd77e7fcb2a086f1185722
TEAM ALISHA <116377135+Team-Alisha@users.noreply.github.com>
2023-05-17 07:51:31 +0530

    Update mongo.py

--- AnonX/core/mongo.py
+++ AnonX/core/mongo.py
@@ -28,5 +28,5 @@ if config.MONGO_DB_URI is None:
 else:
     _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
     _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
-    mongodb = _mongo_async_.Kaal
-    pymongodb = _mongo_sync_.Kaal
+    mongodb = _mongo_async_.Anon
+    pymongodb = _mongo_sync_.Anon
